282efb2afc2fd6e3281b93d5ad69a80eba0d1389
Björn Albers <bjoernalbers@gmail.com>
2018-09-18 11:16:16 +0200

    Import morris.js.
    Add [morris.js](http://morrisjs.github.io/morris.js/) via RubyGem
    [morrisjs-rails](https://github.com/beanieboi/morrisjs-rails).
    This will be used to draw fancy graphs.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require raphael
+//= require morris
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
