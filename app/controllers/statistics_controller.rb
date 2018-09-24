666d8cdac949a666bcb1fc5504623900a99b67dd
Björn Albers <bjoernalbers@gmail.com>
2018-09-24 13:53:57 +0200

    Display avg. appointment waiting periods as graph.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,6 +3,7 @@ class StatisticsController < ApplicationController
     @start = params[:start].present? ? params[:start].to_date : Time.zone.now.beginning_of_month.to_date
     @stop = params[:stop].present? ? params[:stop].to_date : Time.zone.now.to_date
     @appointments = Appointment.chart_data(@start, @stop)
+    @appointment_average_waiting_periods = Appointment.average_waiting_periods(@start, @stop)
     @visits = Visit.chart_data(@start, @stop)
     @patientdetails = Patientdetails.chart_data(@start, @stop)
   end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -25,4 +25,29 @@ class Appointment < ApplicationRecord
       order('anzahl_termine desc').
       limit(10)
   end
+
+  def self.average_waiting_periods(start, stop)
+    # Raw SQL:
+    # SELECT
+    #   ROUND(EXTRACT(EPOCH FROM AVG(termin.beginn - termin.angelegt))/(60*60*24)) as durchschnittliche_wartezeit,
+    #   terminart.bezeichnung as bezeichnung
+    # FROM
+    #   "termin"
+    # INNER JOIN
+    #   "terminart" ON "terminart"."ident" = "termin"."terminart_ident"
+    # WHERE
+    #   "termin"."angelegt" BETWEEN '2018-09-01 00:00:00' AND '2018-09-24 23:59:59.999999'
+    # GROUP BY
+    #   terminart.bezeichnung
+    # ORDER BY
+    #   durchschnittliche_wartezeit DESC
+    # LIMIT 10
+    # ;
+    joins(:calendar).
+      where('termin.angelegt' => start.to_date.beginning_of_day..stop.to_date.end_of_day).
+      group('terminart.bezeichnung').
+      select('ROUND(EXTRACT(EPOCH FROM AVG(termin.beginn - termin.angelegt))/(60*60*24)) as durchschnittliche_wartezeit, terminart.bezeichnung as bezeichnung').
+      order('durchschnittliche_wartezeit desc').
+      limit(10)
+  end
 end
